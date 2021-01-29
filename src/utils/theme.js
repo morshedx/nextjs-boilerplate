c20cb2e4513eb8b2d0db81c222ec691e7e84df77
M Salauddin Morshed <morshedx@gmail.com>
2021-01-29 12:29:48 +0600

    update responsive breakpoints

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,8 +1,9 @@
 export default {
   colors: {
     text: '#111',
+    textAlt: '#343D48',
     background: '#fff',
-    primary: 'tomato',
+    primary: '#EA3A60',
     secondary: '#3f3f3f',
     muted: '#e0e0e0',
     highlight: '#9f9f9f',
@@ -10,6 +11,61 @@ export default {
     accent: '#3f3f3f',
   },
   fonts: {
-    body: 'Comic Sans MS',
+    body: 'DM Sans',
   },
-}
+  space: [
+    0, // 0
+    5, // 1
+    10, // 2
+    15, // 3
+    20, // 4
+    25, // 5
+    30, // 6
+    35, // 7
+    40, // 8
+    45, // 9
+    50, // 10
+    55, // 11
+    60, // 12
+    65, // 13
+    70, // 14
+    75, // 15
+    80, // 16
+    85, // 17
+    90, // 18
+    95, // 19
+    100, // 20
+  ],
+  breakpoints: [
+    // 0
+    '480px', // 1
+    '640px', // 2
+    '768px', // 3
+    '1024px', // 4
+    '1280px', // 5
+    '1366px', // 6
+    '1440px', // 7
+    '1600px', // 8
+    '1920px', // 9
+  ],
+  layout: {
+    container: {
+      maxWidth: [
+        null, // 0
+        null, // 1
+        null, // 2
+        null, // 3
+        960, // 4
+        1170, // 5
+        1260, // 6
+        1366, // 7
+        1440, // 8
+        1720, // 9
+      ],
+      paddingLeft: [5],
+      paddingRight: [5],
+      mx: 'auto',
+    },
+    main: {},
+  },
+};
