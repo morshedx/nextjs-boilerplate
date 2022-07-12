b6bfecce7ebafe953203fa127e801e06ef448eca
morshedx <morshedx@users.noreply.github.com>
2022-07-12 16:39:10 +0600

    added new select componenty

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import { Fragment } from 'react';
 import NextSeo from 'components/next-seo';
+import Select from 'components/ui/select';
 
 const Home = () => {
   return (
     <Fragment>
       <NextSeo />
       <h1 className="text-3xl font-bold underline">Hello world! with TailwindCSS</h1>
+      <Select />
     </Fragment>
   );
 };
